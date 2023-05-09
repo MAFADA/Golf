b6710b261d844f2022a1c78c8846dcd0fcb56f16
M. AFADA NUR SAIVA SYAHIRA <afadanursaiva2001@gmail.com>
2023-05-09 12:00:49 +0700

    Update NextLevelButton.cs
    Update code for Dynamic NextLevelButton load scene

--- Assets/NextLevelButton.cs
+++ Assets/NextLevelButton.cs
@@ -5,20 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class NextLevelButton : MonoBehaviour
 {
-    // private void OnEnable() {
-    //     // check next scene available or not, (false = hide button)
-    //     var currentSccene = SceneManager.GetActiveScene();
-    //     int currentLevel = int.Parse(currentSccene.name.Split("Level ")[1]);
-    //     int nextLevel = currentLevel+1;
-    //     var nextScene = SceneManager.GetSceneByName("Level "+nextLevel);
+    private void OnEnable()
+    {
+        // check next scene available or not, (false = hide button)
+        var currentSccene = SceneManager.GetActiveScene();
+        int currentLevel = int.Parse(currentSccene.name.Split("Level ")[1]);
+        int nextLevel = currentLevel + 1;
+        var nextSceneBuildIndex = SceneUtility.GetBuildIndexByScenePath("Level " + nextLevel);
 
-    //     if (nextScene == null)
-    //     {
-    //         this.gameObject.SetActive(false);
-    //     }
-    // }
-    
-    public void NextLevel(string nextLevelName){
-        SceneManager.LoadScene(nextLevelName);
+        if (nextSceneBuildIndex == -1)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    public void NextLevel()
+    {
+        var currentSccene = SceneManager.GetActiveScene();
+        int currentLevel = int.Parse(currentSccene.name.Split("Level ")[1]);
+        int nextLevel = currentLevel + 1;
+        SceneManager.LoadScene("Level " +nextLevel);
+        
     }
 }
